cd5f92ba7980d53cae093102dcbba39a1a4b68a4
ossinova <oscar.dyremyhr@gmail.com>
2023-11-11 23:03:02 +0100

    addeed meta for author

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ const Document: NextPage<DocumentProps> = () => {
         <link rel="apple-touch-icon" href="/apple-icon.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta httpEquiv="content-type" content="text/html; charset=utf-8" />
-
+        <meta name="author" content="Oscar Dyremyhr"/>
         <script
           async
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`}
