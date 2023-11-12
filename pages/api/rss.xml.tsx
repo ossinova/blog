95d121122d3fb1d13be6242f426e4a150d4799d7
ossinova <oscar.dyremyhr@gmail.com>
2023-11-12 01:42:14 +0100

    new rss

--- pages/api/rss.xml.tsx
+++ pages/api/rss.xml.tsx
@@ -4,8 +4,8 @@ import prisma from '@/lib/prisma';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const feed = new RSS({
-        title: "Your Site Title",
-        description: "Description of your site or feed",
+        title: "Oscar Dyremyhr",
+        description: "Portyfolio and Blog",
         site_url: "https://oscardyremyhr.me/",
         feed_url: "https://oscardyremyhr.me/rss.xml",
         pubDate: new Date(),
@@ -30,6 +30,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             description: post.teaser ?? 'No description available',
             date: post.publishedAt,
             categories: [post.category ?? 'No category available'],
+            author:  "Oscar Dyremyhr",
         });
     });
 
