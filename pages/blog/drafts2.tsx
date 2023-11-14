79347fedd31c9e9813bace6df06919b06c077b38
ossinova <oscar.dyremyhr@gmail.com>
2023-11-14 21:11:23 +0100

    aaa

--- pages/blog/drafts2.tsx
+++ pages/blog/drafts2.tsx
@@ -1,84 +1,84 @@
-import { FC, useEffect, useState } from 'react';
-import prisma from '@/lib/prisma';
-import Container from '@/components/Container';
-import BlogStyles from '@/components/BlogStyles';
-import BlogPost from '@/components/BlogPost';
-import { adminContent } from '@/data/content';
-import { PostProps } from '@/types/post';
-import { GetServerSideProps } from 'next';
-import { useSession, getSession } from 'next-auth/react';
+// import { FC, useEffect, useState } from 'react';
+// import prisma from '@/lib/prisma';
+// import Container from '@/components/Container';
+// import BlogStyles from '@/components/BlogStyles';
+// import BlogPost from '@/components/BlogPost';
+// import { adminContent } from '@/data/content';
+// import { PostProps } from '@/types/post';
+// import { GetServerSideProps } from 'next';
+// import { useSession, getSession } from 'next-auth/react';
 
-type DraftsProps = {
-  drafts: PostProps[];
-};
+// type DraftsProps = {
+//   drafts: PostProps[];
+// };
 
-const Drafts: FC<DraftsProps> = ({ drafts: initialDrafts }) => {
-  const { data: session, status } = useSession();
-  const [drafts, setDrafts] = useState(initialDrafts);
+// const Drafts: FC<DraftsProps> = ({ drafts: initialDrafts }) => {
+//   const { data: session, status } = useSession();
+//   const [drafts, setDrafts] = useState(initialDrafts);
 
-  // Declare fetchDrafts as a function expression
-  const fetchDrafts = async () => {
-    if (status === "authenticated") {
-      try {
-        const fetchedDrafts = await prisma.post.findMany({
-          where: {
-            author: { email: session?.user?.email },
-            published: false,
-          },
-        });
+//   // Declare fetchDrafts as a function expression
+//   const fetchDrafts = async () => {
+//     if (status === "authenticated") {
+//       try {
+//         const fetchedDrafts = await prisma.post.findMany({
+//           where: {
+//             author: { email: session?.user?.email },
+//             published: false,
+//           },
+//         });
 
-        setDrafts(fetchedDrafts); // Update the state with fetched drafts
-      } catch (error) {
-        console.error('Error fetching drafts:', error);
-      }
-    }
-  };
+//         setDrafts(fetchedDrafts); // Update the state with fetched drafts
+//       } catch (error) {
+//         console.error('Error fetching drafts:', error);
+//       }
+//     }
+//   };
 
-  useEffect(() => {
-    fetchDrafts();
-  }, [session, status]); // Add status to dependency array
+//   useEffect(() => {
+//     fetchDrafts();
+//   }, [session, status]); // Add status to dependency array
 
-  return (
-    <Container title={adminContent.drafts.meta.title} robots="noindex">
-      <BlogStyles>
-        <div className="blog admin drafts">
-          <h3>{adminContent.drafts.meta.title}</h3>
-          {drafts.map((draft) => (
-            <div key={draft.id} className="postDraft">
-              <BlogPost post={draft} />
-            </div>
-          ))}
-        </div>
-      </BlogStyles>
-    </Container>
-  );
-};
+//   return (
+//     <Container title={adminContent.drafts.meta.title} robots="noindex">
+//       <BlogStyles>
+//         <div className="blog admin drafts">
+//           <h3>{adminContent.drafts.meta.title}</h3>
+//           {drafts.map((draft) => (
+//             <div key={draft.id} className="postDraft">
+//               <BlogPost post={draft} />
+//             </div>
+//           ))}
+//         </div>
+//       </BlogStyles>
+//     </Container>
+//   );
+// };
 
-export default Drafts;
+// export default Drafts;
 
-export const getServerSideProps: GetServerSideProps = async ({ req }) => {
-  try {
-    const session = await getSession({ req });
-    console.log('Session:', session);
+// export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+//   try {
+//     const session = await getSession({ req });
+//     console.log('Session:', session);
 
-    if (!session) {
-      return { props: { drafts: [] } };
-    }
+//     if (!session) {
+//       return { props: { drafts: [] } };
+//     }
 
-    const drafts = await prisma.post.findMany({
-      where: {
-        author: { email: session?.user?.email },
-        published: false,
-      },
-    });
+//     const drafts = await prisma.post.findMany({
+//       where: {
+//         author: { email: session?.user?.email },
+//         published: false,
+//       },
+//     });
 
-    return {
-      props: {
-        drafts: JSON.parse(JSON.stringify(drafts)),
-      },
-    };
-  } catch (error) {
-    console.error('Error fetching drafts:', error);
-    return { props: { drafts: [] } };
-  }
-};
+//     return {
+//       props: {
+//         drafts: JSON.parse(JSON.stringify(drafts)),
+//       },
+//     };
+//   } catch (error) {
+//     console.error('Error fetching drafts:', error);
+//     return { props: { drafts: [] } };
+//   }
+// };
