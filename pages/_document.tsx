8a18e4aad8a6e5d3ff90b7e5569ba27ba329424f
ossinova <oscar.dyremyhr@gmail.com>
2023-11-19 01:52:50 +0100

    Added assistant

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -25,6 +25,19 @@ const Document: NextPage<DocumentProps> = () => {
             `,
           }}
         />
+         <script dangerouslySetInnerHTML={{ __html: `
+            (function(d, t) {
+                var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+                v.onload = function() {
+                  window.voiceflow.chat.load({
+                    verify: { projectID: '6559400ff587c021f85e9ba5' },
+                    url: 'https://general-runtime.voiceflow.com',
+                    versionID: 'production'
+                  });
+                }
+                v.src = "https://cdn.voiceflow.com/widget/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+            })(document, 'script');
+          `}} />
       </Head>
       <body>
         <script
